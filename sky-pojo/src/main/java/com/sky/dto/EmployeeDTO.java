8a9d19a646b63ff3d571a2ecacc5955459c8401d
Breuky <2779101394@qq.com>
2026-07-15 20:11:25 +0800

    新增员工

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,5 +1,7 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -7,16 +9,22 @@ import java.io.Serializable;
 @Data
 public class EmployeeDTO implements Serializable {
 
+    @ApiModelProperty(value = "主键")
     private Long id;
 
+    @ApiModelProperty(value = "用户名")
     private String username;
 
+    @ApiModelProperty(value = "姓名")
     private String name;
 
+    @ApiModelProperty(value = "手机号")
     private String phone;
 
+    @ApiModelProperty(value = "性别")
     private String sex;
 
+    @ApiModelProperty(value = "身份证号")
     private String idNumber;
 
 }
